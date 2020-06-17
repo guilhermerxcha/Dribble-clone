3485eb6e9c4f5b1bc50dd9d5753b78d3da80aa99
Guilherme Rocha <guilhermerxcha@gmail.com>
2020-06-17 16:05:29 -0300

    criando migration e setando no Shot

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_175414) do
+ActiveRecord::Schema.define(version: 2020_06_17_190441) do
 
   create_table "shots", force: :cascade do |t|
     t.string "title"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_175414) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "shot"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
